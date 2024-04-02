2123962dc69ab475acc430dd162ab62729c51d4b
Samed <samed@canvasheroes.com>
2024-04-02 13:07:30 +0200

    Installed scss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <p>Verhalen Website</p>
+    </main>
+  );
 }
