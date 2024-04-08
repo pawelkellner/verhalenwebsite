544e00e05b175a3cfcee0662be7dea1b092ae128
Samed <samed@canvasheroes.com>
2024-04-08 12:34:16 +0200

    Added necessary inputs

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -7,9 +7,23 @@ export const metadata = {
 };
 
 export default function Write() {
+  const formAction = async () => {
+    "use server";
+    console.log("a");
+  };
   return (
     <main>
-      <PageTitle title="Schrijven" />
+      <PageTitle title="Schrijf jouw verhaal" />
+      <form action={formAction}>
+        <input type="text" name="story_author" />
+        <input type="text" name="story_title" />
+        <input type="text" name="song_title" />
+        <input type="text" name="link_to_song" />
+        <input type="text" name="link_to_song" />
+        <input type="file" accept="image/png, image/jpeg" />
+        <textarea name="quote_text" cols={30} rows={5}></textarea>
+        <input type="text" name="quote_author" />
+      </form>
     </main>
   );
 }
