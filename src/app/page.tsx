687caa0ad837817890d6b219b8885a3781fd0860
Samed <samed@canvasheroes.com>
2024-04-08 15:38:28 +0200

    Moved header to layout file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import PageTitle from "../components/page-title/page-title";
-import Navigation from "../components/navigation/navigation";
 
 export default function Home() {
   return (
-    <>
-      <Navigation></Navigation>
-      <main className="container">
-        <PageTitle noTopPadding title="Recente verhalen" />
-      </main>
-    </>
+    <main className="container">
+      <PageTitle noTopPadding title="Recente verhalen" />
+    </main>
   );
 }
