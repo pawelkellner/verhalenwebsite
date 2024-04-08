432d32f7a0565801c5bf4389014ba31acef54588
TiesHogenboom <tieshogenboom03@gmail.com>
2024-04-08 12:01:17 +0200

    Order verhalen based on first created

--- src/components/firebase/firebaseComponents/ShowItem.tsx
+++ src/components/firebase/firebaseComponents/ShowItem.tsx
@@ -1,20 +1,23 @@
 'use client'
 import React, { useState, useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, orderBy } from "firebase/firestore";
 import { db } from "../../../firebase";
 import "./addItem.css";
 
 export default function AddItem() {
     const [verhalen, setVerhalen] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     const fetchVerhalen = async () => {
+        setLoading(true);
         try {
-            const querySnapshot = await getDocs(collection(db, 'verhalen'));
+            const querySnapshot = await getDocs(query(collection(db, 'verhalen'), orderBy('createdAt', 'asc')));
             const verhalenData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
             setVerhalen(verhalenData);
         } catch (error) {
             console.error("Error fetching documents: ", error);
         }
+        setLoading(false);
     };
 
     useEffect(() => {
@@ -24,15 +27,20 @@ export default function AddItem() {
     return (
         <div className="container">
             <h2>Verhalen</h2>
-            <ul>
-                {verhalen.map(verhaal => (
-                    <li key={verhaal.id}>
-                        <p>Name: {verhaal.name}</p>
-                        <p>Number: {verhaal.number}</p>
-                    </li>
-                ))}
-            </ul>
-            <button onClick={fetchVerhalen}>Load verhalen</button>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <ul>
+                    {verhalen.map(verhaal => (
+                        <li key={verhaal.id}>
+                            <p>Name: {verhaal.name}</p>
+                            <p>Number: {verhaal.number}</p>
+                        </li>
+                    ))}
+                </ul>
+            )}
+            <button onClick={fetchVerhalen} disabled={loading}>Load verhalen</button>
         </div>
     );
-}
+};
+
