01fa0ce6f1c74824a2ac6c3adf39e0fdf4935ff9
TiesHogenboom <tieshogenboom03@gmail.com>
2024-04-08 14:49:29 +0200

    Added create account

--- src/components/firebase/FirebasePage.tsx
+++ src/components/firebase/FirebasePage.tsx
@@ -2,6 +2,7 @@ import AddItem from "./firestoreComponents/AddItem";
 import ShowItem from "./firestoreComponents/ShowItem";
 import DeleteItem from "./firestoreComponents/DeleteItem";
 import EditItem from "./firestoreComponents/EditItem";
+import AuthCreateAccount from "./authComponents/AuthCreateAccount";
 
 export default function FirebasePage() {
     return (
@@ -10,6 +11,7 @@ export default function FirebasePage() {
             <ShowItem></ShowItem>
             <EditItem></EditItem>
             <DeleteItem></DeleteItem>
+            <AuthCreateAccount></AuthCreateAccount>
         </div>
     );
 }
