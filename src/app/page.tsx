22342cade3bb92ab2d5ad595a2da278784ba5eff
TiesHogenboom <tieshogenboom03@gmail.com>
2024-04-09 11:53:52 +0200

    removed firebase from page.tsx for merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import styles from "./page.module.scss";
 import Paragraph from "../components/typography/paragraph";
-import FirebasePage from "../components/firebase/FirebasePage";
 
 export default function Home() {
 
   return (
     <main className={styles.main}>
       <Paragraph variant="sm">Hello</Paragraph>
-      <FirebasePage></FirebasePage>
     </main>
   );
 }
