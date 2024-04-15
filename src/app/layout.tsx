4ca769a5aae6f9db64f337b1e1a1fbe27c37613a
Samed <samed@canvasheroes.com>
2024-04-15 13:44:11 +0200

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,10 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <Header />
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
