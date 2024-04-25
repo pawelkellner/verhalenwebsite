67a3ae52f72cb857b60ab7421fd7dd5902a5213a
Pawel Kellner <56456387+pawelkellner@users.noreply.github.com>
2024-04-25 13:39:31 +0200

    story page layout done

--- src/components/page-title/page-title.tsx
+++ src/components/page-title/page-title.tsx
@@ -7,22 +7,26 @@ import MusicSvg from "../svg/MusicSvg";
 const PageTitle = ({
   title,
   noTopPadding,
+  storyPage,
   paddingBottom,
   songTitle
 }: {
   title: string;
   noTopPadding?: boolean;
   paddingBottom?: boolean;
+  storyPage?: boolean;
   songTitle?: string;
 }) => {
   return (
     <div className={`${!noTopPadding && styles.top__padding}`} data-padding-bottom={paddingBottom && true}>
-        <div className={styles.title_wrapper}>
+        <div className={styles.title_wrapper} data-story-page={storyPage && true}>
             { songTitle ? (
             <>
                 <Heading variant="lg">{title}</Heading>
                 <div>
+                  <div className={styles.scrollingText__container}>
                     <Paragraph variant="md" >{songTitle}</Paragraph>
+                  </div>
                     <MusicSvg/>
                 </div>
             </>
---
+++ src/components/svg/PlayButtonSvg.tsx
@@ -0,0 +1,16 @@
+const PlayButtonSvg = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={38}
+        height={44}
+        fill="none"
+    >
+        <path
+            fill="#FAF9F6"
+            fillRule="evenodd"
+            d="M34.68 16.175 9.576.894C5.372-1.663.142 1.572.142 6.719v30.562c0 5.154 5.23 8.382 9.433 5.825l25.106-15.274c4.234-2.577 4.234-9.08 0-11.657Z"
+            clipRule="evenodd"
+        />
+    </svg>
+)
+export default PlayButtonSvg;
