4d625298d40aab9879fa5a17edd1977c322e8987
Samed <samed@canvasheroes.com>
2024-05-01 18:14:07 +0200

    About done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+
+import style from "./page.module.scss";
+
 import MainLayout from "../../components/main-layout/main-layout";
 import PageTitle from "../../components/page-title/page-title";
+import Heading from "../../components/typography/heading";
+import Paragraph from "../../components/typography/paragraph";
 
 export const metadata = {
   title: "Over",
@@ -11,6 +16,35 @@ export default function About() {
   return (
     <MainLayout>
       <PageTitle title="Over Muziek Verhalen" />
+      <div className={style.about__container}>
+        <Heading variant="lg" fontWeight={500}>
+          Lorem ipsum
+        </Heading>
+        <Paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Paragraph>
+        <Paragraph>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
+          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
+          ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
+          ea voluptate velit esse quam nihil molestiae consequatur, vel illum
+          qui dolorem eum fugiat quo voluptas nulla pariatur?
+        </Paragraph>
+      </div>
     </MainLayout>
   );
 }
