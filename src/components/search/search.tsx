75f123e31a3c7205b86a11871b783af648918021
Samed <samed@canvasheroes.com>
2024-05-02 11:49:57 +0200

    Navigate functionality

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { useRouter } from "next/navigation";
 
 import styles from "./search.module.scss";
 
 import SearchSvg from "../svg/SearchSvg";
 
 const Search = () => {
-  function search(e) {}
+  const [searchTerm, setSearchTerm] = useState("");
+  const router = useRouter();
+
+  const handleSearchInput = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm.trim() !== "") {
+      router.push(`/stories/${encodeURIComponent(searchTerm)}`);
+    }
+  };
 
   return (
-    <div className={styles.searchWrapper}>
+    <form onSubmit={handleSearchSubmit} className={styles.searchWrapper}>
       <input
-        onInput={(e) => search(e)}
+        value={searchTerm}
+        onChange={handleSearchInput}
         type="text"
         placeholder="Zoek verhalen"
       />
-      <SearchSvg iconColor="#7D7D7B" />
-    </div>
+      <button className="unstyled" type="submit">
+        <SearchSvg iconColor="#7D7D7B" />
+      </button>
+    </form>
   );
 };
 
