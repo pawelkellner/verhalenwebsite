5fea9ee16ec2fb67130edb08f6b7a8d5f589579b
Pawel Kellner <56456387+pawelkellner@users.noreply.github.com>
2024-05-13 10:40:37 +0200

    page loading with props

--- src/app/story/[slug]/page.tsx
+++ src/app/story/[slug]/page.tsx
@@ -38,52 +38,7 @@ export default async function Story({ params }: { params: { slug: string } }) {
             <div className={styles.story__titleMobile}>
               <Heading>Peter en de Powerpoint</Heading>
             </div>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
-            aperiam at atque cupiditate, debitis dignissimos dolorum enim esse
-            exercitationem facere fugiat hic iure libero maiores minus molestiae
-            nihil perferendis quas ratione sit tempora vitae voluptatem
-            voluptatibus. Aspernatur commodi, libero. <br />
-            <br />
-            Officia quis repellat unde voluptas. Adipisci cum dolore eum maxime
-            necessitatibus perspiciatis quae sapiente. Animi, consectetur
-            consequuntur deleniti dolorem error expedita ipsum iusto maiores
-            minus mollitia, nam necessitatibus non officiis qui quos recusandae
-            repellat repudiandae, sapiente unde vero! Alias atque dolorem, illum
-            iste itaque quasi sunt vero! Beatae eaque enim id illo laboriosam.
-            Deleniti dolor doloremque eligendi enim exercitationem maiores modi
-            molestias officia, quo ratione reprehenderit sapiente ullam
-            voluptas? Ad cupiditate delectus dignissimos dolorum eaque
-            exercitationem incidunt ipsa libero, molestias neque numquam omnis
-            porro possimus quibusdam quidem repellendus sapiente sit? A
-            architecto dolor dolorum neque non qui soluta! A accusamus ad
-            assumenda dignissimos dolorem error expedita, explicabo fugiat.
-            <br />
-            <br />
-            Hic illo magnam minima, neque nesciunt nihil nisi obcaecati
-            reiciendis reprehenderit ullam?
-            <br />
-            <br />
-            Amet cupiditate eius fugit laborum odit officia pariatur sed
-            veritatis. Ea repellat reprehenderit sunt! Ad beatae, consequatur
-            culpa deleniti doloribus ducimus eaque, et ex fugit nam nesciunt
-            nobis nostrum numquam officiis quibusdam ratione, reprehenderit
-            saepe sed sequi similique sint tenetur voluptates? Alias beatae
-            corporis doloribus obcaecati quae quis quos recusandae rerum
-            voluptatibus. Alias aperiam, commodi corporis dolor dolore dolores
-            earum et eum, excepturi fugit illum incidunt ipsa laudantium
-            repellendus, unde.
-            <br />
-            <br />
-            Alias atque debitis dolorum eaque eligendi et eum eveniet facere
-            harum illo maxime minima minus modi neque nobis non nulla numquam
-            perspiciatis possimus quaerat, quasi quia quis repellendus
-            repudiandae sapiente similique sunt totam ut vitae voluptates!
-            <br />
-            <br />
-            Animi aperiam asperiores atque beatae commodi corporis cum dicta
-            doloremque ea eligendi enim expedita fuga id illo ipsum laborum
-            laudantium magni maiores nisi nostrum nulla, omnis perspiciatis quae
-            qui quia quidem quod quos sunt vero!
+            <div dangerouslySetInnerHTML={{ __html: story?.storyText}}></div>
           </div>
           <div className={styles.story__information}>
               <div className={styles.story__origin}>
@@ -131,66 +86,7 @@ export default async function Story({ params }: { params: { slug: string } }) {
             <Paragraph variant="md">
               Songtekst van &apos;{story?.songTitle}&apos;
             </Paragraph>
-            <div className={styles.story__lyrics}>
-              All we ever hear from you is blah, blah, blah
-              <br />
-              So, all we ever do is go ya, ya, ya
-              <br />
-              And we don&apos;t even care about what they say
-              <br />
-              &apos;Cause it&apos;s ya-ya, ya-ya
-              <br />
-              Blah-blah, blah-blah
-              <br />
-              <br />
-              All we ever hear from you is blah, blah, blah <br />
-              So, all we ever do is go ya, ya, ya <br />
-              And we don&apos;t even care about what they say <br />
-              &apos;Cause it&apos;s ya-ya, ya-ya <br />
-              Blah-blah, blah-blah
-              <br />
-              <br />
-              All we ever hear from you is <br />
-              Blah, blah, blah, blah, blah, blah
-              <br />
-              Blah, blah, blah, blah, blah, blah <br />
-              Blah, blah, blah, blah, blah, blah...
-              <br />
-              <br />
-              All we ever hear from you is blah, blah, blah <br />
-              So, all we ever do is go ya, ya, ya <br />
-              And we don&apos;t even care about what they say <br />
-              &apos;Cause it&apos;s ya-ya, ya-ya <br />
-              Blah-blah, blah-blah
-              <br />
-              <br />
-              All we ever hear from you is blah, blah, blah <br />
-              So, all we ever do is go ya, ya, ya <br />
-              And we don&apos;t even care about what they say <br />
-              &apos;Cause it&apos;s ya-ya, ya-ya <br />
-              Blah-blah, blah-blah
-              <br />
-              <br />
-              All we ever hear from you is blah, blah, blah <br />
-              So, all we ever do is go ya, ya, ya <br />
-              And we don&apos;t even care about what they say <br />
-              &apos;Cause it&apos;s ya-ya, ya-ya <br />
-              Blah-blah, blah-blah
-              <br />
-              <br />
-              All we ever hear from you is blah, blah, blah <br />
-              So, all we ever do is go ya, ya, ya <br />
-              And we don&apos;t even care about what they say <br />
-              &apos;Cause it&apos;s ya-ya, ya-ya <br />
-              Blah-blah, blah-blah
-              <br />
-              <br />
-              All we ever hear from you is blah, blah, blah <br />
-              So, all we ever do is go ya, ya, ya <br />
-              And we don&apos;t even care about what they say <br />
-              &apos;Cause it&apos;s ya-ya, ya-ya <br />
-              Blah-blah, blah-blah
-            </div>
+            <div dangerouslySetInnerHTML={{__html: story?.songText}} className={styles.story__lyrics}></div>
           </div>
         </MainLayout>
       </div>
