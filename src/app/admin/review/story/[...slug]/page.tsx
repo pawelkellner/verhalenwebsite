c62d310a6c6a9ed45b1a293372625c62ae670d96
TiesHogenboom <tieshogenboom03@gmail.com>
2024-05-21 15:01:50 +0200

    Can now approve / delete story in admin dashboard

--- src/app/admin/review/story/[...slug]/page.tsx
+++ src/app/admin/review/story/[...slug]/page.tsx
@@ -94,7 +94,7 @@ export default async function Story({ params }: { params: { slug: string } }) {
         </MainLayout>
       </div>
       <MainLayout>
-        <AdminButtons slug={slug} />
+        <AdminButtons slug={slug} story={story} />
       </MainLayout>
     </>
   );
--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -78,7 +78,7 @@ const Form = () => {
           originText: originText,
           storyText: storyText,
           songText: songText,
-          underReview: false
+          underReview: true
         };
 
         let imageUrl: string | null = null;
