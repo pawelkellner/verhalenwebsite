83b073217342e75a8683e9db9a1b62bd2593d637
Pawel Kellner <56456387+pawelkellner@users.noreply.github.com>
2024-05-27 14:33:03 +0200

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,17 +4,17 @@ import type { NextRequest } from 'next/server';
 import { isUserLoggedIn } from './app/actions';
 
 export async function middleware(request: NextRequest) {
-    try {
-        const isAuthenticated = await isUserLoggedIn();
-        if (!isAuthenticated) {
-            return NextResponse.redirect(new URL('/admin', request.url));
-        }
-
-    } catch (error) {
-        console.error(error);
-    }
-
-    return NextResponse.next();
+    // try {
+    //     const isAuthenticated = await isUserLoggedIn();
+    //     if (!isAuthenticated) {
+    //         return NextResponse.redirect(new URL('/admin', request.url));
+    //     }
+    //
+    // } catch (error) {
+    //     console.error(error);
+    // }
+    //
+    // return NextResponse.next();
 }
 
 export const config = {
