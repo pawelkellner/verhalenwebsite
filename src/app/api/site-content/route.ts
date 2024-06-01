cc4bb83af064e39e09e315a7d3d0f1a84c02c829
Pawel Kellner <56456387+pawelkellner@users.noreply.github.com>
2024-06-01 10:46:21 +0200

    fix?

--- src/app/api/site-content/route.ts
+++ src/app/api/site-content/route.ts
@@ -1,6 +1,7 @@
-import { collection, getDocs, query, orderBy } from "firebase/firestore";
+import {collection, getDocs, query, orderBy, addDoc, serverTimestamp} from "firebase/firestore";
 import { db } from "../../../firebase";
 import { SiteContent } from "../../../utils";
+import {getDownloadURL, getStorage, ref, uploadBytes} from "firebase/storage";
 
 export async function GET(request: Request) {
     console.log(request)
@@ -21,6 +22,43 @@ export async function GET(request: Request) {
         })
     } catch (error) {
         return new Response(JSON.stringify(error), {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            status: 400,
+        })
+    }
+}
+
+export async function POST(request: Request) {
+    const data = await request.json();
+
+    try {
+        let imageUrl: string | null = null;
+
+        if (data.songImage) {
+            const storage = getStorage();
+            const storageRef = ref(storage, data.songImage.name);
+            await uploadBytes(storageRef, data.songImage);
+            imageUrl = await getDownloadURL(storageRef);
+        }
+
+        const docRef = await addDoc(collection(db, "verhalen"), {
+            ...data.story,
+            songImage: imageUrl,
+            createdAt: serverTimestamp()
+        });
+
+        return new Response(JSON.stringify({
+            message: `Document written with ID: ${docRef.id}`
+        }), {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            status: 201,
+        })
+    } catch (e) {
+        return new Response(JSON.stringify(e), {
             headers: {
                 "Content-Type": "application/json",
             },
