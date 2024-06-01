d4cb2d9e06c0578250d22837028cc958bba8801c
Pawel Kellner <56456387+pawelkellner@users.noreply.github.com>
2024-06-01 10:11:04 +0200

    fix?

--- src/app/api/site-content/route.ts
+++ src/app/api/site-content/route.ts
@@ -2,7 +2,8 @@ import { collection, getDocs, query, orderBy } from "firebase/firestore";
 import { db } from "../../../firebase";
 import { SiteContent } from "../../../utils";
 
-export async function GET() {
+export async function GET(request: Request) {
+    console.log(request)
     try {
         const querySnapshot = await getDocs(query(collection(db, 'siteContent'), orderBy('createdAt', 'asc')));
         const siteData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() } as SiteContent));
